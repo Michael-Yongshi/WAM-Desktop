d3843ff5241ba21664c9b91159a13d73209606cc
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-08-22 17:12:27 +0200

    logging doesnt work

--- cli.py
+++ cli.py
@@ -1,6 +1,25 @@
+import os
+import logging
+
 from gui.main import (
     run,
 )
 
 if __name__ == '__main__':
-    run()
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(message)s',
+        handlers=[
+            logging.FileHandler(os.path.join(os.path.expanduser(os.path.join("~", "Documents", "WAM")), "session.log")),
+            logging.StreamHandler()
+            ]
+        )
+    logging.debug('This message should go to the log file and to the console')
+    logging.info('So should this')
+    logging.warning('And this, too')
+
+    try:
+        run() # app()
+    except:
+        logging.exception('Exception caught')
+        raise
