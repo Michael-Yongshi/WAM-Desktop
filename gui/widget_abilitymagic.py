ba664254f026dc509e1cbf7bf4d636713b84226f
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2021-06-11 19:37:41 +0200

    cleaned up imports

--- gui/widget_abilitymagic.py
+++ gui/widget_abilitymagic.py
@@ -1,56 +1,21 @@
 
-from os import name
-from PyQt5.QtCore import (
-    Qt,
-    pyqtSignal,
-    )
 
 from PyQt5.QtWidgets import (
-    QApplication,
-    QFrame,
-    QGridLayout,
-    QHBoxLayout,
     QInputDialog,
-    QLabel,
-    QMainWindow,
     QMessageBox,
-    QProgressBar,
-    QPushButton, 
-    QSizePolicy,
     QVBoxLayout,
-    QWidget, 
-    )
-
-from PyQt5.QtGui import (
-    QFont,
-    QFontDatabase,
-    QIcon,
     )
 
 from wamcore.core.methods_engine import (
-    save_warband,
-    load_warband,
-    show_warbands,
-    save_reference,
     load_reference,
     )
 
 from wamcore.core.class_hierarchy import (
-    Warband,
-    Squad,
-    Character,
-    Hero,
-    Henchman,
-    Rule,
-    Treasury,
-    Item,
-    Skill,
     Ability,
     Magic,
     )
 
 from darktheme.widget_template import *
-
 
 
 class WidgetAbility(QBorderlessFrame):
